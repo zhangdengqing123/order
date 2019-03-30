3caeb7c7c301276a60a487a2093aa0713d9a0a94
Daniel <daniel@danieldeMacBook-Pro.local>
2019-03-30 21:06:48 +0800

    new ratings file and img

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ratings">
+  <div class="ratings" ref="ratings">
     <div class="ratings-content">
       <div class="overview">
         <div class="overview-left">
@@ -25,14 +25,50 @@
         </div>
       </div>
       <split></split>
+      <ratingselect
+         @increment="onSelectType"
+         @toggle="toggleContent"
+         :onlyContent="onlyContent"
+         :selecType="selecType"
+         :ratings="ratings">
+      </ratingselect>
+      <div class="ratings-wrapper">
+        <ul>
+          <li v-show="needShow(rating.rateType, rating.text)" v-for="(rating, index) of ratings" :key="index" class="rating-item border-1px-t">
+            <div class="avatar">
+              <img width="28" height="28" :src="rating.avatar"/>
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                <detail-star class="star" :size="24" :score="rating.score"></detail-star>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}分钟送达</span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                <span class="icon-thumb_up"></span>
+                <span class="recommend-item" v-for="(item, index) of rating.recommend" :key="index">
+                  {{item}}
+                </span>
+              </div>
+              <div class="time">{{rating.rateTime | formatDateY}}</div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
 import Star from '@/components/star/star'
 import Split from '@/components/split/split'
+import { formatDate } from '@/common/js/date'
 import axios from 'axios'
+import RatingSelect from '@/components/ratingselect/ratingselect'
+const All = 2
+
 export default {
   name: 'v-ratings',
   props: {
@@ -40,9 +76,22 @@ export default {
       type: Object
     }
   },
+  data () {
+    return {
+      selecType: All,
+      onlyContent: true
+    }
+  },
+  filters: {
+    formatDateY (time) {
+      let date = new Date(time)
+      return formatDate(date, 'yyyy-MM-dd hh-mm')
+    }
+  },
   components: {
     'detail-star': Star,
-    'split': Split
+    'split': Split,
+    'ratingselect': RatingSelect
   },
   methods: {
     getRatingsInfo () {
@@ -55,6 +104,33 @@ export default {
       if (res.errno === ERR_OK && res.data) {
         let data = res.data
         this.$store.state.ratings = data
+        this.$nextTick(() => {
+          this.scroll = new BScroll(this.$refs.ratings, {
+            click: true
+          })
+        })
+      }
+    },
+    onSelectType (type) {
+      this.selecType = type
+      this.$nextTick(() => {
+        this.scroll.refresh()
+      })
+    },
+    toggleContent () {
+      this.onlyContent = !this.onlyContent
+      this.$nextTick(() => {
+        this.scroll.refresh()
+      })
+    },
+    needShow (type, text) {
+      if (this.onlyContent && !text) { // 判断只显示评价有内容，没有直接内容直接跳过
+        return false
+      }
+      if (this.selecType === All) { // 判断显示所有内容，有或没有都显示
+        return true
+      } else {
+        return type === this.selecType // 判断选择类型是否匹配
       }
     }
   },
@@ -70,8 +146,14 @@ export default {
 </script>
 
 <style lang="less" scoped>
+@import '../../common/less/mixni.less';
 .ratings {
+  position: absolute;
+  top: 174px;
+  bottom: 0;
+  left: 0;
   width: 100%;
+  overflow: hidden;
   .ratings-content {
     .overview{
       display: flex;
@@ -133,6 +215,86 @@ export default {
             display: inline-block;
             line-height: .9rem;
             font-size: .6rem;
+            color: rgb(147, 153, 159);
+          }
+        }
+      }
+    }
+    .ratings-wrapper {
+      padding: 0 .9rem;
+      .rating-item {
+        display: flex;
+        padding: .9rem 0;
+        .border-1px-t(rgba(7, 17, 27, .1));
+        .avatar {
+          flex: 0 0 1.4rem;
+          width: 1.4rem;
+          margin-right: @12px;
+          img {
+            border-radius: 50%;
+          }
+        }
+        .content {
+          position: relative;
+          flex: 1;
+          .name {
+            margin-bottom: .2rem;
+            line-height: @12px;
+            font-size: @10px;
+            color: rgb(7, 17, 27);
+          }
+          .star-wrapper {
+            display: flex;
+            margin-bottom: .3rem;
+            font-size: 0;
+            .star {
+              display: inline-block;
+              vertical-align: top;
+            }
+            .delivery {
+              display: inline-block;
+              margin-left: .3rem;
+              vertical-align: top;
+              line-height: @12px;
+              font-size: @10px;
+              font-weight: @fw200;
+              color: rgb(147, 153, 159);
+            }
+          }
+          .text {
+            margin-bottom: .4rem;
+            line-height: .9rem;
+            font-size: @12px;
+            color: rgb(7, 17, 27);
+          }
+          .recommend {
+            font-size: 0;
+            .icon-thumb_up {
+              display: inline-block;
+              margin-right: .3rem;
+              line-height: .8rem;
+              font-size: .6rem;
+              color: rgb(0, 160, 220);
+            }
+            .recommend-item {
+              display: inline-block;
+              line-height: .8rem;
+              margin:0 .4rem .2rem 0;
+              padding: 0 .3rem;
+              border: solid 1px rgba(7, 17, 27, .1);
+              border-radius: 1px;
+              font-size: .45rem;
+              background: #fff;
+              color: rgb(147, 153, 159);
+            }
+          }
+          .time {
+            position: absolute;
+            top: 0;
+            right: 0;
+            line-height: @12px;
+            font-size: @10px;
+            font-weight: @fw200;
             color: rgb(147, 153, 159);
           }
         }
--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,15 +1,228 @@
 <template>
-  <div class="seller">
-     商家
+  <div class="seller" ref="seller">
+    <div class="seller-content">
+      <div class="overview">
+        <h1 class="title">{{seller.name}}</h1>
+        <div class="desc border-1px-b">
+          <star :size="36" :score="seller.score"></star>
+          <span class="text">({{seller.ratingCount}})</span>
+          <span class="text">月售{{seller.sellCount}}单</span>
+        </div>
+        <div class="remark">
+          <div class="block">
+            <span class="title">起送价</span>
+            <div class="content">
+              <span class="stress">{{seller.minPrice}}</span>
+              <span class="unit">元</span>
+            </div>
+          </div>
+          <div class="block">
+            <span class="title">商家配送</span>
+            <div class="content">
+              <span class="stress">{{seller.deliveryPrice}}</span>
+              <span class="unit">元</span>
+            </div>
+          </div>
+          <div class="block">
+            <span class="title">平均配送时间</span>
+            <div class="content">
+              <span class="stress">{{seller.deliveryTime}}</span>
+              <span class="unit">分钟</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <split></split>
+      <div class="bulletin">
+        <h1 class="title">公告与活动</h1>
+        <div class="content-wrapper border-1px-t">
+          <p class="content">{{seller.bulletin}}</p>
+        </div>
+        <ul v-if="seller.supports" class="supports">
+           <li class="supports-item" v-for="(item, index) of seller.supports" :key="index">
+              <span class="icons" :class="classMap[item.type]"></span>
+              <span class="text">{{item.description}}</span>
+           </li>
+         </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
+import star from '@/components/star/star'
+import split from '@/components/split/split'
+import axios from 'axios'
 export default {
-  name: 'v-seller'
+  name: 'v-seller',
+  components: {
+    star,
+    split
+  },
+  methods: {
+    getSellerInfo () {
+      axios.get('/api/seller')
+        .then(this.getSellerInfoSucc)
+    },
+    getSellerInfoSucc (response) {
+      const ERR_OK = 0
+      const res = response.data
+      if (res.errno === ERR_OK && res.data) {
+        let data = res.data
+        this.$store.state.seller = data
+      }
+    }
+  },
+  computed: {
+    seller () {
+      return this.$store.state.seller
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.seller, {
+      click: true
+    })
+  },
+  created () {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+    this.getSellerInfo()
+  }
 }
 </script>
 
 <style lang="less" scoped>
+@import '../../common/less/mixni.less';
 
+.seller {
+  position: absolute;
+  top: 174px;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  .seller-content {
+    .overview {
+      padding: .9rem;
+      .title {
+        margin-bottom: .4rem;
+        line-height: .7rem;
+        font-size: .7rem;
+        color: rgb(7, 17, 27);
+      }
+      .desc {
+        padding-bottom:.9rem;
+        font-size: 0;
+        .border-1px-t(rgba(7, 17, 27, 0.1));
+        .star {
+          display: inline-block;
+          vertical-align: top;
+        }
+        .text {
+          display: inline-block;
+          vertical-align: top;
+          line-height: .9rem;
+          font-size: .5rem;
+          color: rgb(77, 85, 93);
+          &:nth-child(2) {
+            margin-left: .4rem;
+          }
+          &:nth-child(3) {
+            margin-left: .6rem;
+          }
+        }
+      }
+      .remark {
+        display: flex;
+        margin-top: .9rem;
+        .block {
+          flex: 1;
+          text-align: center;
+          border-right: solid 1px rgba(7, 17, 27, 0.1);
+          .title {
+            line-height: .5rem;
+            font-size: .5rem;
+            color: rgb(147, 153, 159);
+          }
+          .content {
+            line-height: 1.2rem;
+            padding-top: .2rem;
+            font-size: 0;
+            .stress {
+              display: inline-block;
+              font-size: 1.2rem;
+              font-weight: 200;
+              color: rgb(7, 17, 27);
+            }
+            .unit {
+              display: inline-block;
+              font-size: .5rem;
+            }
+          }
+          &:last-child {
+            border: none;
+          }
+        }
+      }
+    }
+    .bulletin {
+      padding: .9rem .9rem 0 .9rem;
+      .title {
+        margin-bottom: .4rem;
+        line-height: .7rem;
+        font-size: .7rem;
+        color: rgb(7, 17, 27);
+      }
+      .content-wrapper {
+        padding: 0 .3rem .6rem .3rem;
+        .border-1px-t(rgba(7, 17, 27, .1));
+        .content {
+          line-height: 1.2rem;
+          text-align: justify;
+          font-weight: 200;
+          font-size: .6rem;
+          color:rgb(240, 20, 20)
+        }
+      }
+    }
+    .supports {
+      .supports-item {
+        padding: .8rem .6rem;
+        .border-1px-t(rgba(7, 17, 27, .1));
+        font-size: 0;
+        .icons {
+          display: inline-block;
+          vertical-align: top;
+          width: @16px;
+          height: @16px;
+          margin-right: .2rem;
+          background-size: @16px @16px;
+          background-repeat: no-repeat;
+          &.decrease {
+            .bg-image('@{sellerUrl}decrease_2');
+          }
+          &.discount {
+            .bg-image('@{sellerUrl}discount_2');
+          }
+          &.special {
+            .bg-image('@{sellerUrl}special_2');
+          }
+          &.invoice {
+            .bg-image('@{sellerUrl}invoice_2');
+          }
+          &.guarantee {
+            .bg-image('@{sellerUrl}guarantee_2');
+          }
+        }
+        .text {
+          vertical-align: top;
+          line-height: @16px;
+          font-size: @12px;
+          font-weight: @fw200;
+          color: rgb(7, 17, 27);
+        }
+      }
+    }
+  }
+}
 </style>
